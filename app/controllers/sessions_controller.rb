873c5e1316f0ef1e91dccb01fbffd851e31a96e1
yoshinator <yoanante@gmail.com>
2019-09-10 16:23:59 -0400

    auth related work

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,41 @@
+class SessionsController < ApplicationController
+    def googleAuth
+        # Get access tokens from the google server
+        access_token = request.env["omniauth.auth"]
+        user = User.from_omniauth(access_token)
+        user.save
+        log_in(user)
+        
+        # Access_token is used to authenticate request made from the rails application to the google server
+        #user.google_token = access_token.credentials.token
+        
+        # Refresh_token to request new access_token
+        # Note: Refresh_token is only sent once during the first request
+        refresh_token = access_token.credentials.refresh_token
+        user.google_refresh_token = refresh_token if refresh_token.present?
+        user.save
+        redirect_to root_path
+  end
+
+  def log_in(user)
+    payload = { data: user }
+
+
+    token = JWT.encode payload, ENV['HMAC_SECRET'], 'HS256'
+
+    # eyJhbGciOiJIUzI1NiJ9.eyJkYXRhIjoidGVzdCJ9.pNIWIL34Jo13LViZAJACzK6Yf0qnvT_BuwOxiMCPE-Y
+    puts token
+
+    decoded_token = JWT.decode token, hmac_secret, true, { algorithm: 'HS256' }
+
+    # Array
+    # [
+    #   {"data"=>"test"}, # payload
+    #   {"alg"=>"HS256"} # header
+    # ]
+    puts "The token is #{token}"
+    puts "The decoded token is #{decoded_token}"
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   resources :users
   get "/", to: "static_pages#index"
+  root :to => "static_pages#index"
 
   # Routes for Google authentication
   get "auth/:provider/callback", to: "sessions#googleAuth"
   get "auth/failure", to: redirect("/")
 
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
