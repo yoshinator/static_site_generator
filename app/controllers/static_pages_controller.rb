878eee193fbf27ed5444858382f3844e23bc1b31
yoshinator <yoanante@gmail.com>
2019-09-10 12:25:46 -0400

    google oauth stuff

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
 
     def index 
-
+        puts ENV
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_10_134512) do
+ActiveRecord::Schema.define(version: 2019_09_10_162452) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2019_09_10_134512) do
     t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "google_token"
+    t.string "google_refresh_token"
   end
 
 end
