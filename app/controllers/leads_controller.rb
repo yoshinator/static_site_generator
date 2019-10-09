fdb2e898d4ebbac27fe5740cf8b0b020c43bcd3a
yoshinator <yoanante@gmail.com>
2019-10-08 21:08:46 -0400

    leads routes and controller

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -1,22 +1,32 @@
 class LeadsController < ApplicationController
+  before_action :set_lead, only: [:show]
   
   def index 
+    @leads = Lead.all
   end 
 
   def create 
-    fail
-    @lead = Lead.new(params[:lead])
+    @lead = Lead.new(f_name: lead_params["f_name"], email: lead_params["email"], phone: lead_params["phone"], street_1: lead_params["street_1"], city: lead_params["city"], zip: lead_params["zip"], business_id: lead_params["business_id"])
+    @lead.addContent(lead_params)
     if @lead.valid? 
       @lead.save 
-      flash[:message] = "Successful submit request"
+      flash[:notice] = "success"
       redirect_to root_path
-    end 
-    flash[:error] = lead.errors
-    redirect_to root_path
+    else 
+      flash[:notice] = "fail"
+      redirect_to root_path
+    end
   end 
+
+  def show  
+  end
 
   private
   def lead_params
-    params.require(:lead).permit(:f_name, :zip, :city, :street_1, :job_location, :time_line)
+    params.require(:lead).permit(:f_name, :zip, :city, :street_1, :job_location, :business_id, :timeline, :email, :phone)
+  end 
+
+  def set_lead
+    @lead = Lead.find(params[:id])
   end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,12 @@ Rails.application.routes.draw do
   resources :users
   resources :pages
   resources :businesses
+  resources :leads
   resource :cities
   resource :services
   get "/", to: "static_pages#index"
   get "/logout", to: "sessions#destroy"
 
-  get "/leads", to: "leads#index"
-  post "/leads", to: "leads#create"
 
   # Routes for Google authentication
   get "auth/:provider/callback", to: "sessions#googleAuth"
