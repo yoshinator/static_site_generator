7f7633761d24313887e83f653900ca906ff39efe
yoshinator <yoanante@gmail.com>
2019-11-14 11:17:41 -0500

    started testing

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,48 +1,50 @@
 require 'test_helper'
 
+
 class UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:one)
+    $stdout.puts @user.attributes
   end
 
-  test "should get index" do
+  test "get index shoudl redirect to root if not logged in" do
     get users_url
+    assert_redirected_to root_path
+  end
+
+  test "get new should redirect to root if not logged in and user not admin" do
+    get new_user_url 
     assert_response :success
   end
 
-  test "should get new" do
-    get new_user_url
-    assert_response :success
-  end
+  # test "should create user" do
+  #   assert_difference('User.count') do
+  #     post users_url, params: { user: { email: @user.email, f_name: @user.f_name, l_name: @user.l_name, username: @user.username } }
+  #   end
 
-  test "should create user" do
-    assert_difference('User.count') do
-      post users_url, params: { user: { email: @user.email, f_name: @user.f_name, l_name: @user.l_name, username: @user.username } }
-    end
+  #   assert_redirected_to user_url(User.last)
+  # end
 
-    assert_redirected_to user_url(User.last)
-  end
-
-  test "should show user" do
+  test "should redirect if user is not loggedin" do
     get user_url(@user)
-    assert_response :success
+    assert_redirected_to root_path
   end
 
-  test "should get edit" do
+  test "edit user should redirect if user is not logged in" do
     get edit_user_url(@user)
-    assert_response :success
+    assert_redirected_to root_path
   end
 
-  test "should update user" do
+  test "update user should redirect if user not logged in" do
     patch user_url(@user), params: { user: { email: @user.email, f_name: @user.f_name, l_name: @user.l_name, username: @user.username } }
-    assert_redirected_to user_url(@user)
+    assert_redirected_to root_path
   end
 
-  test "should destroy user" do
-    assert_difference('User.count', -1) do
+  test "destroy user should redirect if user not logged in" do
+    assert_difference('User.count', 0) do
       delete user_url(@user)
     end
-
-    assert_redirected_to users_url
+  # assert_redirected_to users_url
+      assert_redirected_to root_path
   end
 end
